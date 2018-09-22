d34e1a584466435715a381380e147e5da191a21e
Michael B. Johnson <wave@pixar.com>
2018-09-21 23:07:20 -0700

    formatting

--- skp2usd/main.cpp
+++ skp2usd/main.cpp
@@ -23,20 +23,28 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+
 #include "USDExporter.h"
 #include "pxr/base/arch/systemInfo.h"
 #include "pxr/base/plug/registry.h"
 
-int
-main(int argc, const char * argv[]) {
-    if (argc < 3) {
-        std::cerr   << "USAGE: " << argv[0]
-        << " <in.skp> <out.usd[a,z]>" << std::endl;
-        return -1;
-    }
-    const std::string skpFile = std::string(argv[1]);
-    const std::string usdFile = std::string(argv[2]);
+/*
+ We'd like to have arguments with defaults:
+ --exportMeshes 1
+ --exportCameras 1
+ --exportMaterials 1
+ --arKitCompatible 1
+ --exportDoubleSided 1
+ --singleFile 0
+ --exportNormals 0
+ --exportCurves 0
+ --exportLines 0
+ --exportEdges 0
+ */
 
+void
+_findUSDPlugins() {
     // we're assuming that this executable has been installed in the
     // USDExporter.plugin/Contents/MacOS/ that is itself installed inside
     // of a SketchUp app bundle. Note that this is almost certainly a
@@ -44,14 +52,58 @@ main(int argc, const char * argv[]) {
     // to deal with this then.
     std::string dir = pxr::TfGetPathName(pxr::ArchGetExecutablePath());
     std::string pluginDir = pxr::TfStringCatPaths(dir,
-    "../Contents/Resources/usd/");
+                                                  "../Contents/Resources/usd/");
     std::cerr << "using USD plugins directory: " << pluginDir << std::endl;
     pxr::PlugRegistry::GetInstance().RegisterPlugins(pluginDir);
+}
 
+
+int
+main(int argc, const char * argv[]) {
+    _findUSDPlugins();
+
+    if (argc < 3) {
+        std::cerr   << "USAGE: " << argv[0]
+        << " [opts] <in.skp> <out.usd[a,z]>" << std::endl;
+        return -1;
+    }
+    const std::string skpFile = std::string(argv[1]);
+    const std::string usdFile = std::string(argv[2]);
+
+    while ((c = getopt (argc, argv, "abc:")) != -1)
+        switch (c)
+    {
+        case 'a':
+
+    // these are the same defaults as the plug-in
+    // we might want to have these returned in a JSON dict or something
+    // so if they changed, we could just call a function to get them.
+    // But for now, hard code them here...
+    bool exportMeshes = true;
+    bool exportCameras = true;
+    bool exportMaterials = true;
+    bool exportDoubleSided = true;
+    bool exportToSingleFile = false;
+    bool exportARKitCompatibleUSDZ = true;
+    bool exportNormals = false;
+    bool exportCurves = false;
+    bool exportLines = false;
+    bool exportEdges = false;
+
+    // we should handle command line args here:
+    
     try {
         auto myExporter = USDExporter();
-        myExporter.SetExportToSingleFile(false);
-        myExporter.SetExportARKitCompatibleUSDZ(true);
+        myExporter.SetExportMeshes(exportMeshes);
+        myExporter.SetExportCameras(exportCameras);
+        myExporter.SetExportMaterials(exportMaterials);
+        myExporter.SetExportDoubleSided(exportDoubleSided);
+        myExporter.SetExportToSingleFile(exportToSingleFile);
+        myExporter.SetExportARKitCompatibleUSDZ(exportARKitCompatibleUSDZ);
+        myExporter.SetExportNormals(exportNormals);
+        myExporter.SetExportCurves(exportCurves);
+        myExporter.SetExportLines(exportLines);
+        myExporter.SetExportEdges(exportEdges);
         if (!myExporter.Convert(skpFile, usdFile, NULL)) {
             std::cerr << "Failed to save USD file " << usdFile << std::endl;
             return -2;
